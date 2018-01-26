39cf2da4e626ad438c2fa82bcf82c7f548ae54c0
Sarah Sexton <sarahse@microsoft.com>
2018-01-26 15:14:37 -0600

    http://hannabot.azurewebsites.net Static Page

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ var tableStorage = new azure.AzureBotStorage({gzipData: false}, azureTableClient
 // Listen for messages from users
 server.post('/api/messages', connector.listen())
 
-server.get('/', restify.serveStatic({
-  directory: __dirname,
-  default: '/index.html'
- }));
+server.get(/\/?.*/, restify.serveStatic({
+  directory: './public',
+  default: 'index.html'
+}))
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector).set('storage', tableStorage)
