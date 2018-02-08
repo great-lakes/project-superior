67e6f43f343cdfb9f84462482ae91b2eb1665756
Kevin Leung <keleung@microsoft.com>
2018-02-08 13:22:40 -0600

    adding services directory to API calls

--- src/dialogs/showSurvey.js
+++ src/dialogs/showSurvey.js
@@ -3,10 +3,13 @@ module.exports = function (bot) {
     function (session, args, next) {
       session.send('Hello, you are in showSurvey dialog!')
       session.endDialog()
-
       // TODO pull from API: Prize and Link
-      // session.send('Make sure to take the survey to enter raffle for a TODO:PRIZE!')
+
+      // Here is the link to the survey
       // session.send('Survey Link: TODO:SURVEY_LINK')
+
+      // Thanks for taking the survey! You will be entered to win
+      // session.send('Make sure to take the survey to enter raffle for a TODO:PRIZE!')
       // session.endDialog()
     }
   ])
