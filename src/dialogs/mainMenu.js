3860681b68ce9ac5f136136a4266f321f4df2467
Kevin Leung <keleung@microsoft.com>
2018-02-08 08:47:30 -0600

    refactored out switch statement

--- src/dialogs/mainMenu.js
+++ src/dialogs/mainMenu.js
@@ -1,12 +1,31 @@
 var builder = require('botbuilder')
 
-const choice1 = 'Get Azure code'
-const choice2 = 'Get technology examples'
-const choice3 = 'Learn about the Microsoft Team'
-const choice4 = 'Ask the team a question'
-const choice5 = 'Azure Code does not work'
-const choice6 = 'Nevermind'
-
+const choicesArr = [
+  {
+    dialogName: 'azureCode',
+    choiceText: 'Get Azure code'
+  },
+  {
+    dialogName: 'techHelp',
+    choiceText: 'Get technology help'
+  },
+  {
+    dialogName: 'teamInfo',
+    choiceText: 'Get to know the Microsoft Team'
+  },
+  {
+    dialogName: 'askQuestion',
+    choiceText: 'Ask the team a question'
+  },
+  {
+    dialogName: 'azureCodeError',
+    choiceText: 'Azure code does not work'
+  },
+  {
+    dialogName: 'endConvo',
+    choiceText: 'Nevermind'
+  }
+]
 module.exports = function (bot) {
   bot.dialog('mainMenu', [
     function (session, args, next) {
@@ -14,30 +33,21 @@ module.exports = function (bot) {
       builder.Prompts.choice(
         session,
         'Select a choice',
-        [choice1, choice2, choice3, choice4, choice5, choice6]
+        choicesArr.map(item => {
+          return item.choiceText
+        })
       )
     },
     function (session, args, next) {
-      switch (args.response.index) {
-        case 0: // Get Azure Code
-          session.beginDialog('azureCode')
-          break
-        case 1: // Get technology examples
-          session.beginDialog('techHelp')
-          break
-        case 2: // Learn about the team
-          session.beginDialog('teamInfo')
-          break
-        case 3: // Ask the team a question
-          session.beginDialog('askQuestion')
-          break
-        case 4: // Azure code Error
-          session.beginDialog('azureCodeError')
-          break
-        case 5: // Nevermind
-          session.replaceDialog('endConvo')
-          return
+      const choiceIndex = args.response.index
+      const { dialogName } = choicesArr[choiceIndex]
+
+      if (dialogName === 'endConvo') {
+        session.replaceDialog('endConvo')
+        return
       }
+
+      session.beginDialog(dialogName)
       session.replaceDialog('isSatisfied')
     }
   ])
