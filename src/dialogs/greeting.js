1dd477aee2a57e974fab18792b6170625d953291
Kevin Leung <keleung@microsoft.com>
2018-02-08 10:08:03 -0600

    completed greetingDialog

--- src/dialogs/greeting.js
+++ src/dialogs/greeting.js
@@ -1,7 +1,9 @@
 module.exports = function (bot) {
   bot.dialog('greeting', [
     function (session, args, next) {
-      session.send('Hello, you are in greeting dialog!')
+      session.send('Hello HackIllinois Hacker!')
+      session.sendTyping()
+      session.send("I'm Hanna, Microsoft's hackathon bot. What can I help you with?")
       session.endDialog()
     }
   ])
